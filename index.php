39bbbdf784c11e1712d7afa9cc6b51651204667f
Justin <justin_pleyto@dlsu.edu.ph>
2015-01-06 23:07:05 +0800

    hello

--- index.php
+++ index.php
@@ -199,7 +199,25 @@
     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
   </div>
 
+  <div class="col-lg-3 col-md-3 col-sm-3">
+    <div class="team">
+        <img class="img-responsive" src="images/t1.jpg" alt="paul" />
+          <div class="roll">
+            <div class="sn-links">
+              <a href="#" title="#"><i class="fa fa-facebook-square"></i></a>
+              <a href="#" title="#"><i class="fa fa-twitter-square"></i></a>
+              <a href="#" title="#"><i class="fa fa-linkedin-square"></i></a><br>
+              <a href="#" title="#"><i class="fa fa-youtube-square"></i></a>
+              <a href="#" title="#"><i class="fa fa-flickr"></i></a>
+              <a href="#" title="#"><i class="fa fa-instagram"></i></a>
+            </div><!-- end sn links -->
+          </div><!-- end roll -->
+    </div>
+    <h2>Chris</h2>
+    <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
 
+  </div>
 </div><!-- end row -->
 
 </div><!-- end container -->
