9a2a45778514e819b51bb8e78dc5e5a6b38b1926
Justin <justin_pleyto@dlsu.edu.ph>
2015-01-06 21:59:18 +0800

    hello

--- index.php
+++ index.php
@@ -80,24 +80,26 @@
         <div class="item active">
            <div class="container">
             <div class="carousel-caption">
-              <h3>Located at Filinvest Alabang, Nyne Corporation specialize in interior fit out of residential, commercial and office spaces.</h3>
-              <p></p>
-            </div>
+                <h1>Dedicated</h1>
+              <p>Being in the construction industry since the mid-nineties, Nyne is committed to high quality of work with the use of skilled labourers and carefully sourced materials.</p>
+
+           </div>
           </div>
         </div>
         <div class="item">
            <div class="container">
             <div class="carousel-caption">
-              <h1>Being in the construction industry since the mid-nineties</h1>
-              <p>Nyne is committed to high quality of work with the use of skilled labourers and carefully sourced materials .</p>
-            </div>
+               <h1>Specialized</h1>
+              <p>Located at Filinvest Alabang, Nyne Corporation specialize in interior fit out of residential, commercial and office spaces.</p>
+
+          </div>
           </div>
         </div>
         <div class="item">
           <div class="container">
             <div class="carousel-caption">
-              <h1>Nyne upholds client’s brief and will complete projects in time and within budget.</h1>
-              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              <h1>Driven</h1>
+              <p>Nyne upholds client’s brief and will complete projects in time and within budget.</p>
             </div>
           </div>
         </div>
