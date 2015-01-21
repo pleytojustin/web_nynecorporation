e1e2b6cf3c81570aa6eddc81c0ae44e4358d36f4
Justin <justin_pleyto@dlsu.edu.ph>
2015-01-21 21:35:20 +0800

    created space

--- index.php
+++ index.php
@@ -538,16 +538,23 @@
   <div class="row">
     <div class="col-lg-10 col-lg-offset-1 text-center">
       <h1>Directions</h1>
+
    </div>
+
   </div>
+
   </div>
-  <div class="map">
+    <div class="map">
 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1932.0598917052182!2d121.04233644907377!3d14.420257735793394!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0x878f75a3fda2ffaf!2sEntrata+Urban+Complex!5e0!3m2!1sen!2sph!4v1419677240994" width="600" height="450" frameborder="0" style="border:0"></iframe>
 </div>
 
+
 </div><!-- end contact -->
 
+<div>
 
+<p> </p>
+</div>
 
 <div class="swiper">
     <div class="swiper-container">
