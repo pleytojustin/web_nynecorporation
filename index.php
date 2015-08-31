ce01cd95fc7ab47094434fc3c328eff7435e54b1
Justin <justin_pleyto@dlsu.edu.ph>
2015-08-31 13:12:52 +0800

    changed title

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 -->
 
 
-<title>NyneCorporation</title>
+<title>JUSTIN PLEYTO</title>
 
 <link href="css/bootstrap.css" rel="stylesheet">
 <link href="fonts/font1.css" rel='stylesheet' type='text/css'>
@@ -52,7 +52,7 @@
       <nav id="navigation" class="navbar navbar-right" role="navigation">
       <div class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#home" title="Home">Homee</a></li>
+            <li class="active"><a href="#home" title="Home">Home</a></li>
             <li class="dropdown"><a href="#about-us" class="dropdown-toggle" data-toggle="dropdown">About </a></li>
             <li><a href="#gallery" title="#">Portfolio</a></li>
             <li><a href="#posts" title="Blog">Services</a></li>
